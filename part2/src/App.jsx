0056e68fb4f1f1797267b1bd157cc5f8c701010e
chitsip149 <minhtrangkl03@gmail.com>
2024-10-14 01:16:35 +0800

    add form and controlled component

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Note from './components/Note'
 
 // const Note = ({note}) => {
@@ -6,13 +7,44 @@ import Note from './components/Note'
 //   )
 // }
 
-const App = ({notes}) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  const handleNoteChange = (event) => { //every time a change occurs in the input element, the event handler function receives the event object as its event parameter
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const addNotes = (event) => {
+    event.preventDefault() //the default action will make the page refresh
+    console.log('button clicked', event.target)
+    const noteObject = {
+      content: newNote,
+      important: Math.random()<0.5,
+      id: String(notes.length+1),
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const noteToShow = showAll?notes:notes.filter(note => note.important)
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll?'important':'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map(note => <Note key={note.id} note={note} />)}
+        {noteToShow.map(note => <Note key={note.id} note={note} />)}
       </ul>
+      <form onSubmit={addNotes}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button> 
+      </form>
     </div>
   )
 }
