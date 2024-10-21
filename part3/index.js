7e84f62e36f02aa12d099122f419f4aa8e823e8a
chitsip149 <minhtrangkl03@gmail.com>
2024-10-21 23:04:34 +0800

    add json server and express dependency

--- part3/index.js
+++ part3/index.js
@@ -1,17 +1,26 @@
-// console.log('hello world')
-
 const http = require('http') // the application imports node's built-in web server module
-
-//the code uses the createServer method of the http module to create a new web server
-//an event handler is registered to the server that is called everytimr an http request is made to
-//the server address http://localhost:3001
-
-//the request is responded to with the status code 200, with the Content-Type header set to text/plain
-//and the content of the site to be set to hello world
+let notes = [
+    {
+        id: "1",
+        content: "HTML is easy",
+        important: true
+      },
+      {
+        id: "2",
+        content: "Browser can execute only JavaScript",
+        important: false
+      },
+      {
+        id: "3",
+        content: "GET and POST are the most important methods of HTTP protocol",
+        important: true
+      }
+    
+]
 
 const app = http.createServer((request, response) => {
-    response.writeHead(200, {'Content-Type': 'text/plain'})
-    response.end("Hello World")
+    response.writeHead(200, {'Content-Type': 'application/json'})
+    response.end(JSON.stringify(notes))
 })
 
 //bind the http server assigned to the app variable, to listen to http requests sent to port 3001
